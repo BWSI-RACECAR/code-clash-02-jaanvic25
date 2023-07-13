5964d8e213b28cf8dc19559bea98fb9e32101173
jaanvi chirimar <92826861+jaanvic25@users.noreply.github.com>
2023-07-13 09:14:29 -0400

    Update digitdecrypt.py

--- digitdecrypt.py
+++ digitdecrypt.py
@@ -30,7 +30,7 @@ Input: 0 Output: 0
 class Solution:    
     def digitdecrypt(self, num):
         rank = 100
-        while len(str(num)) > 0:
+        while len(str(rank)) > 0:
             rank = num
             leng = len(str(num)) 
             for i in range(rank):
